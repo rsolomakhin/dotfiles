3c93bdcb4cbb3054e636fee43521f855106b3a84
Rouslan Solomakhin <rouslan.solomakhin@gmail.com>
2015-03-08 00:43:03 -0800

    Fixup install.bat.

--- install.bat
+++ install.bat
@@ -1,7 +1,7 @@
-rmdir /S/Q              %USERPROFILE%\vimfiles
-xcopy /Y/S vim          %USERPROFILE%\vimfiles
-copy  /Y   cvsignore    %USERPROFILE%\.cvsignore
-copy  /Y   gitconfig    %USERPROFILE%\.gitconfig
-copy  /Y   bashrc       %USERPROFILE%\.bashrc
-copy  /Y   bash_aliases %USERPROFILE%\.bash_aliases
-copy  /Y   profile      %USERPROFILE%\.profile
+rmdir /S/Q                %USERPROFILE%\vimfiles
+xcopy /Y/S/I vim          %USERPROFILE%\vimfiles
+copy  /Y     cvsignore    %USERPROFILE%\.cvsignore
+copy  /Y     gitconfig    %USERPROFILE%\.gitconfig
+copy  /Y     bashrc       %USERPROFILE%\.bashrc
+copy  /Y     bash_aliases %USERPROFILE%\.bash_aliases
+copy  /Y     profile      %USERPROFILE%\.profile
