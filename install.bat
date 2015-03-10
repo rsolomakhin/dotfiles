e4dbe7242879cd1601debd3d5cf06932fbe198fe
Rouslan Solomakhin <rouslan.solomakhin@gmail.com>
2015-03-09 17:52:25 -0700

    Clean up unused files.

--- install.bat
+++ install.bat
@@ -1,7 +1,9 @@
-rmdir /S/Q                %USERPROFILE%\vimfiles
-xcopy /Y/S/I vim          %USERPROFILE%\vimfiles
-copy  /Y     vimrc        %USERPROFILE%\.vimrc
+copy  /Y     bash_profile %USERPROFILE%\.bash_profile
+copy  /Y     bashrc       %USERPROFILE%\.bashrc
 copy  /Y     cvsignore    %USERPROFILE%\.cvsignore
 copy  /Y     gitconfig    %USERPROFILE%\.gitconfig
-copy  /Y     bashrc       %USERPROFILE%\.bashrc
-copy  /Y     profile      %USERPROFILE%\.bash_profile
+copy  /Y     vimrc        %USERPROFILE%\.vimrc
+del                       %USERPROFILE%\.bash_aliases
+del                       %USERPROFILE%\.profile
+rmdir /S/Q                %USERPROFILE%\vimfiles
+xcopy /Y/S/I vim          %USERPROFILE%\vimfiles
