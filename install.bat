de9932f91f69bba6dfca112c27799529b0d6807d
Rouslan Solomakhin <rouslan.solomakhin@gmail.com>
2015-04-20 09:39:08 -0700

    Remove bash.

--- install.bat
+++ install.bat
@@ -1,5 +1,3 @@
-copy  /Y     bash_profile %USERPROFILE%\.bash_profile
-copy  /Y     bashrc       %USERPROFILE%\.bashrc
 copy  /Y     cvsignore    %USERPROFILE%\.cvsignore
 copy  /Y     gitconfig    %USERPROFILE%\.gitconfig
 copy  /Y     vimrc        %USERPROFILE%\.vimrc
