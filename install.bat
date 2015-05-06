02606303296b4b472de435817f44a383c0902767
Rouslan Solomakhin <rouslan.solomakhin@gmail.com>
2015-05-05 17:58:29 -0700

    install emacs config too

--- install.bat
+++ install.bat
@@ -1,4 +1,5 @@
 copy  /Y     cvsignore    %USERPROFILE%\.cvsignore
+copy  /Y     emacs        %USERPROFILE%\.emacs
 copy  /Y     gitconfig    %USERPROFILE%\.gitconfig
 copy  /Y     vimrc        %USERPROFILE%\.vimrc
 copy  /Y     zshrc        %USERPROFILE%\.zshrc
