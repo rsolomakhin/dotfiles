5d616fcb6e7dd44869ea15cc1bb7802a3f774379
Rouslan Solomakhin <rouslan.solomakhin@gmail.com>
2015-06-21 15:26:14 -0700

    Overwrite previous links.

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ die() {
 
 for file in common.sh cvsignore emacs.d/init.el env-android.sh env-clang.sh env-cros.sh env-gn.sh env-lsan.sh fluxbox/apps fluxbox/menu fluxbox/startup fluxbox/init fluxbox/keys fluxbox/windowmenu screenrc tmux.conf vimrc zshrc Xresources bashrc bash_profile; do
   mkdir -p ~/`dirname $file` || die "Cannot create the dir for $file"
-  ln -s ~/.dotfiles/$file ~/.$file || die "Cannot link $file to home dir"
+  ln -sfv ~/.dotfiles/$file ~/.$file || die "Cannot link $file to home dir"
 done
 
 git config user.email "rouslan.solomakhin@gmail.com" || die "Cannot set git user email for this repository"
