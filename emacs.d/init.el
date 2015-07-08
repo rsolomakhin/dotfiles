13b4308d9e02934505e25b7cdd08f127eb9988ee
Rouslan Solomakhin <rouslan.solomakhin@gmail.com>
2015-07-07 18:31:41 -0700

    Do not mess with status line color from the theme.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -113,10 +113,3 @@
  '(vc-initial-comment t)
  '(whitespace-line-column nil)
  '(whitespace-style (quote (trailing space-before-tab empty space-after-tab face lines))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(mode-line ((t (:background "grey75" :foreground "black"))))
- '(mode-line-inactive ((t (:inherit mode-line :background "grey90" :foreground "grey20" :weight light)))))
