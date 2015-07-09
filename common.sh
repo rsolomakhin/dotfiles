6162b69bd63defeb020c9ffbc2edd76773bd05b2
Rouslan Solomakhin <rouslan.solomakhin@gmail.com>
2015-07-08 18:10:22 -0700

    Do not load site file when starting emacs. That's as fast as starting emacs client.

--- common.sh
+++ common.sh
@@ -16,7 +16,7 @@ export ALTERNATE_EDITOR=""
 export ANDROID_HOME=$HOME/android-sdk-linux
 export CHROME_DEVEL_SANDBOX=/usr/local/sbin/chrome-devel-sandbox
 export CHROMIUM=$HOME/chrome/src
-export EDITOR="emacsclient -t"
+export EDITOR="emacs --no-site-file"
 export FZF_CTRL_T_COMMAND="find -L . \( \
       -path '*/\.*' -o \
       -path '\./out*' -o \
@@ -74,7 +74,7 @@ if ! ssh-add -l | grep /.ssh/ >& /dev/null; then
 fi
 
 alias e="$EDITOR"
-alias em="emacsclient -t"
+alias em="emacs --no-site-file"
 alias grep="grep --color=auto"
 alias j="jobs"
 alias so="source"
