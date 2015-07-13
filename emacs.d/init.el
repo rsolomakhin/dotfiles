deb836e3b352591877e0d603abcf6fea6420ded2
Rouslan Solomakhin <rouslan.solomakhin@gmail.com>
2015-07-13 15:50:40 -0700

    Add back company-mode load path, as that adds at most 20ms to startup time.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -12,6 +12,8 @@
 ;; See the License for the specific language governing permissions and
 ;; limitations under the License.
 
+(add-to-list 'load-path "~/.emacs.d/lisp/company-mode")
+
 (autoload
   'clang-format
   "~/.emacs.d/lisp/clang-format/clang-format"
