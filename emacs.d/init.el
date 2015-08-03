ac95161fc12cb0a052085447849d9f07e0b212bb
Rouslan Solomakhin <rouslan.solomakhin@gmail.com>
2015-08-03 12:39:14 -0700

    Make all C like languages more convient to edit.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -44,8 +44,8 @@
 (add-to-list 'auto-mode-alist '("\\.h" . c++-mode))
 (add-to-list 'auto-mode-alist '("\\.mm" . objc-mode))
 
-(add-hook 'cpp-mode-hook 'google-set-c-style)
-(add-hook 'cpp-mode-hook 'google-make-newline-indent)
+(add-hook 'c-mode-common-hook 'google-set-c-style)
+(add-hook 'c-mode-common-hook 'google-make-newline-indent)
 (add-hook 'java-mode-hook
           (lambda ()
             (setq c-basic-offset 4)
