816039e368c2a3d668ca18000602d30ecf20bb34
Rouslan Solomakhin <rouslan.solomakhin@gmail.com>
2016-06-05 16:35:36 -0700

    Fix the windows install script

--- windows/install.bat
+++ windows/install.bat
@@ -14,6 +14,33 @@
 
 @echo off
 
+if not exist %SYSTEMDRIVE%\src mkdir %SYSTEMDRIVE%\src ^
+  || echo "Cannot create src dir" && exit /b 1
+if not exist %USERPROFILE%\vimfiles mkdir %USERPROFILE%\vimfiles ^
+  || echo "Cannot create vim dir" && exit /b 1
+if not exist %USERPROFILE%\vimfiles\spell mkdir %USERPROFILE%\vimfiles\spell ^
+  || echo "Cannot create vim spell dir" && exit /b 1
+if not exist %USERPROFILE%\vimfiles\swap mkdir %USERPROFILE%\vimfiles\swap ^
+  || echo "Cannot create vim swap dir" && exit /b 1
+if not exist %USERPROFILE%\vimfiles\autoload mkdir %USERPROFILE%\vimfiles\autoload ^
+  || echo "Cannot create vim autoload dir" && exit /b 1
+
+bitsadmin.exe /transfer "DownloadVimPathogen" https://tpo.pe/pathongen.vim ^
+  %USERPROFILE%\vimfiles\autoload\pathogen.vim ^
+  || echo "Cannot download vim pathogen" && exit /b 1
+
+pushd %~dp0 ^
+  || echo "Cannot go into the directory of the install script" && exit /b 1
+
+copy /Y drmemory-env.bat %SYSTEMDRIVE%\src\ ^
+  || echo "Cannot copy drmemory-env.bat" && exit /b 1
+copy /Y env.bat %SYSTEMDRIVE%\src\ ^
+  || echo "Cannot copy env.bat" && exit /b 1
+copy /Y ..\cvsignore %USERPROFILE%\.cvsignore ^
+  || echo "Cannot copy cvsignore" && exit /b 1
+copy /Y ..\vimrc %USERPROFILE%\.vimrc ^
+  || echo "Cannot copy vim config" && exit /b 1
+
 git.exe config --global --replace-all alias.br branch ^
   || echo "Cannot set git br alias" && exit /b 1
 git.exe config --global --replace-all alias.brv "branch -vv" ^
@@ -57,34 +84,8 @@ git.exe config --replace-all user.email "rouslan.solomakhin@gmail.com" ^
 git.exe submodule update --init --recursive ^
   || echo "Cannot update submodules" && exit /b 1
 
-if not exist %SYSTEMDRIVE%\src mkdir %SYSTEMDRIVE%\src ^
-  || echo "Cannot create src dir" && exit /b 1
-
-pushd %~dp0 ^
-  || echo "Cannot go into the directory of the install script" && exit /b 1
-
-copy /Y drmemory-env.bat %SYSTEMDRIVE%\src\ ^
-  || echo "Cannot copy drmemory-env.bat" && exit /b 1
-copy /Y env.bat %SYSTEMDRIVE%\src\ ^
-  || echo "Cannot copy env.bat" && exit /b 1
-
-copy /Y ..\cvsignore %USERPROFILE%\.cvsignore ^
-  || echo "Cannot copy cvsignore" && exit /b 1
-xcopy /Y ..\vimrc %USERPROFILE%\.vimrc ^
-  || echo "Cannot copy vim config" && exit /b 1
 xcopy /Y/S/I ..\vim %USERPROFILE%\vimfiles ^
   || echo "Cannot copy vimfiles dir" && exit /b 1
 
 popd ^
   || echo "Cannot return to original directory" && exit /b 1
-
-mkdir %USERPROFILE%\vimfiles\spell ^
-  || echo "Cannot create vim spell dir" && exit /b 1
-mkdir %USERPROFILE%\vimfiles\swap ^
-  || echo "Cannot create vim swap dir" && exit /b 1
-mkdir %USERPROFILE%\vimfiles\autoload ^
-  || echo "Cannot create vim autoload dir" && exit /b 1
-
-bitsadmin.exe /transfer "DownloadVimPathogen" https://tpo.pe/pathongen.vim ^
-  %USERPROFILE%\vimfiles\autoload\pathogen.vim ^
-  || echo "Cannot download vim pathogen" && exit /b 1
